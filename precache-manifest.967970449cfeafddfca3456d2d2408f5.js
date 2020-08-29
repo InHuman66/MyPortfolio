7c86d1e971185fb3b30dd66e8935a368d1f52f9f
InHuman66 <olexandr.kk@gmail.com>
2020-08-29 10:00:35 +0300

    Updates

---
+++ precache-manifest.967970449cfeafddfca3456d2d2408f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f71b0bdc2b3ef6e3ef638bc55047170",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "d475ca0b509a5a3d2ba0",
+    "url": "/MyPortfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "990edd2a7a004bbb8806",
+    "url": "/MyPortfolio/static/css/main.d238e124.chunk.css"
+  },
+  {
+    "revision": "d475ca0b509a5a3d2ba0",
+    "url": "/MyPortfolio/static/js/2.58e9a814.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.58e9a814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990edd2a7a004bbb8806",
+    "url": "/MyPortfolio/static/js/main.1bbc99f8.chunk.js"
+  },
+  {
+    "revision": "82f4c39aa2689e80dc5e",
+    "url": "/MyPortfolio/static/js/runtime-main.e8be6c19.js"
+  },
+  {
+    "revision": "0d5548f92225f4c6ed60afb237890b68",
+    "url": "/MyPortfolio/static/media/icons8-react-native-512.0d5548f9.png"
+  }
+]);
